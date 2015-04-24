df6a04e3916153638f8025b307b94e8b219e6d79
Jonas Sjöberg <jomeganas@gmail.com>
2015-04-24 22:05:06 +0200

    Start rewriting the install script..
    This script needs a thorough fixup to be even remotely reliable.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,28 +2,27 @@
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles
-# in ~/dotfiles						http://blog.smalleycreative.com/tutorials/
+# in ~/dotfiles                     http://blog.smalleycreative.com/tutorials/
 # using-git-and-github-to-manage-your-dotfiles/
 ############################
 
-########## Variables
+## DENNA
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
- # list of files/folders to symlink in homedir
+dotfiles_dir="$(pwd)"                  # assume we are in the dotfiles directory
+dotfiles_old=""$dotfiles_dir""/dotfiles_old""    # old dotfiles backup directory
+
+# These files are _the_ dotfiles
 files="bashrc vimrc vim zshrc oh-my-zsh gitconfig"   
 
-##########
-
 # create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
+echo "Creating $dotfiles_old for backup of any existing dotfiles in "$HOME" .."
+mkdir -pv "$dotfiles_old"
+echo ".. done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
+echo "Changing to the $dotfiles_dir directory .."
+cd $dotfiles_dir
+echo ".. done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
