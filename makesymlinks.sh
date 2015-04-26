cdea668d9052425ccb0bc813d95d3074154f9122
Jonas Sjöberg <jomeganas@gmail.com>
2015-04-26 08:37:08 +0200

    Update makesymlinks.sh.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 #   Should only ever need to be executed after a clean reinstall of the home
 #   directory.  Replaces the standard "/home/user/.configfile"-files with
-#   symlinks to files kept under version control.in this here repository.
+#   symlinks to files kept under version control in this here repository.
 #   Some error checks are performed but please note that I WILL NOT take any
 #   responsability for anything you might do with this script. Be careful.
 #
@@ -16,7 +16,7 @@
 #cd "$(dirname "$0")/.."             # this probably lands us in $HOME
 DOTFILES_ROOT=$(pwd)
 TEMP_DIR="$HOME/temporary_dotfiles"
-BACKUP_DIR="$HOME/"
+BACKUP_ARCHIVE="$HOME/dotfiles_$(date +%Y-%m-%d_%H-%M-%S).tar.gz" 
 
 # These files are *the* dotfiles!
 CONFIGFILES="bashrc vimrc vim zshrc oh-my-zsh gitconfig"
@@ -28,7 +28,9 @@ cat << EOF
   =====================
 
 * started with root "$DOTFILES_ROOT"
-* backing up any .dotfiles already in "$HOME" ..
+* creating symlinks from "$HOME" to "$DOTFILES_ROOT"
+  for dotfiles: "$CONFIGFILES"
+* current dotfiles will be backed up to "$BACKUP_ARCHIVE"
 
 EOF
 
@@ -46,7 +48,7 @@ do
 
     if [ -f "$DOTFILES_ROOT/$file" ];
     then
-        echo "* creating symlink to "$DOTFILES_ROOT/$file" .."
+        echo "* creating symlink from "$HOME/.$file" to "$DOTFILES_ROOT/$file" .."
         ln -vsi "$DOTFILES_ROOT/$file" "$HOME/.$file"
         echo ""
     fi
@@ -60,7 +62,8 @@ cat << EOF
 
 EOF
 
-tar vcfz ""$BACKUP_DIR"dotfiles-$(date +%Y-%m-%d_%H-%M-%S).tar.gz" "$TEMP_DIR"
+tar vcfz "$BACKUP_ARCHIVE" -C "$TEMP_DIR" .
+
 #
 # # create dotfiles_old in homedir
 # echo "Creating $dotfiles_old for backup of any existing dotfiles in "$HOME" .."
