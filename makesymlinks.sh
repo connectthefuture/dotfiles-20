b8aef876b683789bad1ca22f81cf62a2248927e5
Jonas Sjöberg <jomeganas@gmail.com>
2015-04-26 10:57:58 +0200

    Update setup script.
    Finishing touches. Still not very portable or safe though.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -3,7 +3,7 @@
 
 #   DOTFILES SETUP SCRIPT
 #                               written by Jonas Sjöberg for *personal* use
-#                                                  Last updated: 2015-04-25
+#                                                  Last updated: 2015-04-26
 #   ______________________________________________________________________
 
 #   Should only ever need to be executed after a clean reinstall of the home
@@ -13,7 +13,6 @@
 #   responsability for anything you might do with this script. Be careful.
 #
 
-#cd "$(dirname "$0")/.."             # this probably lands us in $HOME
 DOTFILES_ROOT=$(pwd)
 TEMP_DIR="$HOME/temporary_dotfiles"
 BACKUP_ARCHIVE="$HOME/dotfiles_$(date +%Y-%m-%d_%H-%M-%S).tar.gz" 
@@ -21,7 +20,7 @@ BACKUP_ARCHIVE="$HOME/dotfiles_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
 # These files are *the* dotfiles!
 CONFIGFILES="bashrc vimrc vim zshrc oh-my-zsh gitconfig"
 
-# print some text
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 cat << EOF
   ──────────────────────────────────────────────────────────────────────────────
   Dotfiles setup script
@@ -41,7 +40,7 @@ for file in $CONFIGFILES;
 do
     if [ -f ".$file" ];                  # check for current config files
     then                                 # if present move to temporary storage
-        echo "* moving ".$file" out of the way .."
+        echo "** moving ".$file" out of the way .."
         mv -vni ".$file" "$TEMP_DIR"        
         echo ""
     fi
@@ -54,35 +53,18 @@ do
     fi
 done
 
-# print some text
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 cat << EOF
 
-** done ..
-** creating .tar.gz archive with old dotfiles ..
+* done ..
+** archiving the old dotfiles ..
 
 EOF
 
+# this assumes more than a minute passes between runs .. file exists error?
 tar vcfz "$BACKUP_ARCHIVE" -C "$TEMP_DIR" .
 
-#
-# # create dotfiles_old in homedir
-# echo "Creating $dotfiles_old for backup of any existing dotfiles in "$HOME" .."
-# mkdir -pv "$dotfiles_old"
-# echo ".. done"
-#
-# # change to the dotfiles directory
-# # echo "Changing to the $dotfiles_dir directory .."
-# # cd $dotfiles_dir
-# # echo ".. done"
-#
-# # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-# for file in $files; do
-#     echo "Moving any existing dotfiles from ~ to $olddir"
-#     echo "mv -v ~/.$file ~/dotfiles_old/"
-#     echo "Creating symlink to $file in home directory."
-#     echo "ln -s ""$dotfilesdir"/"$file"" ~/."$file""
-# done
-#
-# # create symlink to zsh theme
-# echo "Symlink zsh theme .."
-# #ln -s $dir/jonas.zsh-theme $dir/oh-my-zsh/themes/jonas.zsh-theme
+
+## create symlink to zsh theme
+#echo "Symlink zsh theme .."
+#ln -s $dir/jonas.zsh-theme $dir/oh-my-zsh/themes/jonas.zsh-theme
