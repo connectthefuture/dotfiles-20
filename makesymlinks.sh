380f4160eeb8c70197977f68dca76702fd60876a
Jonas Sjöberg <jomeganas@gmail.com>
2015-04-26 17:10:05 +0200

    Add symlinking thunar configuration file to setup.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -16,6 +16,7 @@
 DOTFILES_ROOT=$(pwd)
 TEMP_DIR="$HOME/temporary_dotfiles"
 BACKUP_ARCHIVE="$HOME/dotfiles_$(date +%Y-%m-%d_%H-%M-%S).tar.gz" 
+MOVE="mv -vni"
 
 # These files are *the* dotfiles!
 CONFIGFILES="bashrc vimrc vim zshrc oh-my-zsh gitconfig"
@@ -41,15 +42,18 @@ do
     if [ -f ".$file" ];                  # check for current config files
     then                                 # if present move to temporary storage
         echo "** moving ".$file" out of the way .."
-        mv -vni ".$file" "$TEMP_DIR"        
+        $MOVE ".$file" "$TEMP_DIR"        
         echo ""
     fi
 
-    if [ -f "$DOTFILES_ROOT/$file" ];
-    then
+    if [ -f "$DOTFILES_ROOT/$file" ];    # check that file is in repo directory
+    then                                 # if not, print error and die
         echo "* creating symlink from "$HOME/.$file" to "$DOTFILES_ROOT/$file" .."
         ln -vsi "$DOTFILES_ROOT/$file" "$HOME/.$file"
         echo ""
+    else
+        echo "** error! $DOTFILES_ROOT/$file doesn't exist!"
+        exit
     fi
 done
 
@@ -63,6 +67,34 @@ EOF
 
 # this assumes more than a minute passes between runs .. file exists error?
 tar vcfz "$BACKUP_ARCHIVE" -C "$TEMP_DIR" .
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+# symlink thunar custom actions config file
+THUNARCONF_DST="$HOME/.config/Thunar/uca.xml"
+THUNARCONF_SRC="$DOTFILES_ROOT/thunar-custom.xml"
+
+if [ -f "$THUNARCONF_SRC" ];
+then
+    echo "$THUNARCONF_SRC exists .."
+
+    if [ -f "$THUNARCONF_DST" ];    # is the config file already in place?
+    then
+        echo "$THUNARCONF_DST exists .."
+        echo "** moving existing file out of the way .."
+        $MOVE "$THUNARCONF_DST" ""$THUNARCONF_DST"_$(date +%Y-%m-%d_%H-%M-%S)"
+        echo ""
+    fi
+
+#   echo "* creating symlink from "$THUNARCONF_DST" to "$THUNARCONF_SRC" .."
+    echo "** creating symlink .."
+    ln -vsi "$THUNARCONF_SRC" "$THUNARCONF_DST"
+    echo ""
+else
+    echo "** error! $THUNARCONF_SRC doesn't exist!"
+    exit
+fi
+
 
 
 ## create symlink to zsh theme
