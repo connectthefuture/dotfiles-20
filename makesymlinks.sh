1a8a00e6fbbed95bcdc3d6554a0162b2b38828a4
Jonas Sjöberg <jomeganas@gmail.com>
2015-06-17 19:07:07 +0200

    Use double square brackets in conditionals.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -60,12 +60,12 @@ do
     THIS_SRC="${HOME}/.${file}"              # this file is a link ..
     THIS_DST="${DOTFILES_ROOT}/${file}"      # .. to this file
 
-    if [ -f "${THIS_SRC}" ]; then
-        if [ -L "${THIS_SRC}" ]; then
+    if [[ -f "${THIS_SRC}" ]]; then
+        if [[ -L "${THIS_SRC}" ]]; then
             echo "** removing symlink .$file .."
             rm -v "${THIS_SRC}"
         else
-            if [ ! -d "${TEMP_DIR}" ]; then
+            if [[ ! -d "${TEMP_DIR}" ]]; then
                 mkdir -pvv "${TEMP_DIR}"   # setup a folder for temporary storage
             fi
 
@@ -74,7 +74,7 @@ do
         fi
     fi
 
-    if [ -e "${THIS_DST}" ];               # check that file is in repo directory
+    if [[ -e "${THIS_DST}" ]];               # check that file is in repo directory
     then                                 # if not, print error and die
         echo "** creating symlink .."    # else create symlink from SRC to DST
         ln -vsi "${THIS_DST}" "${THIS_SRC}"
@@ -91,7 +91,7 @@ printf "\n* done ..\n"
 # exists. If it does, we can assume it contains our old dotfiles.
 # Create a zipped tar archive with a date and timestamp in the filename.
 # Then go ahead aand remove the temporary directory and files.
-if [ -d "${TEMP_DIR}" ]; then
+if [[ -d "${TEMP_DIR}" ]]; then
     printf "\n** archiving the old dotfiles ..\n"
 
     # this assumes more than a minute passes between runs .. file exists error?
@@ -127,9 +127,9 @@ if command -v thunar >/dev/null; then
 
 EOF
 
-    if [ -f "${THUNARCONF_DST}" ]; then         # is the repo file in place?
-        if [ -f "${THUNARCONF_SRC}" ]; then     # is the config file already in place?
-            if [ -L "${THUNARCONF_SRC}" ]; then # is the config file a symlink?
+    if [[ -f "${THUNARCONF_DST}" ]]; then         # is the repo file in place?
+        if [[ -f "${THUNARCONF_SRC}" ]]; then     # is the config file already in place?
+            if [[ -L "${THUNARCONF_SRC}" ]]; then # is the config file a symlink?
                 # It's a symlink. Just remove it.
                 echo "* removing symlink ${THUNARCONF_SRC} .."
                 rm -v "${THUNARCONF_SRC}"
@@ -161,7 +161,7 @@ fi
 echo "Symlinking zsh theme .."
 OHMYZSH_THEMES="${DOTFILES_ROOT}/oh-my-zsh/themes"
 
-if [ -d "${OHMYZSH_THEMES}" ]; then
+if [[ -d "${OHMYZSH_THEMES}" ]]; then
     ln -vsi "${DOTFILES_ROOT}/jonas.zsh-theme" \
           "${OHMYZSH_THEMES}/jonas.zsh-theme"
 else
